34734a37b532bc8a48d90b13d830ffb6312d184e
Arun Kumar Devarajulu <arun89@terpmail.umd.edu>
2019-06-04 22:16:05 -0400

    Added new test case

--- test/test.cpp
+++ test/test.cpp
@@ -7,6 +7,15 @@ TEST(AdditionTest, FractionAdd1) {
   Fraction<int> b(3, 15);
   Fraction<int> c;
   c = a + b;
-  EXPECT_EQ(c.get_numerator(), 5);
-  EXPECT_EQ(c.get_denominator(), 15);
+  EXPECT_EQ(c.get_numerator(), 1);
+  EXPECT_EQ(c.get_denominator(), 3);
+}
+
+TEST(AdditionTest, FractionAdd2) {
+  Fraction<int> a(-2, 15);
+  Fraction<int> b(-9, 7);
+  Fraction<int> c;
+  c = a + b;
+  EXPECT_EQ(c.get_numerator(), -149);
+  EXPECT_EQ(c.get_denominator(), 105);
 }
