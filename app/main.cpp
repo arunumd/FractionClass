b334aeefb93afc19c409296e88c087d7eecc19c4
Arun Kumar <arun89@terpmail.umd.edu>
2019-06-05 19:02:03 -0400

    Added definitions for copy constructor and assignment operator of Fraction class

--- app/main.cpp
+++ app/main.cpp
@@ -1,16 +1,24 @@
+/*!
+ * @file main.cpp
+ * @author Arun Kumar Devarajulu
+ * @license This project is released under the MIT License.
+ * @date May 28, 2019
+ * @brief This file oontains the implementation of the Fraction class project
+ */
+
 #include <iostream>
-#include "../include/Fraction.h"
-#include "../include/Fraction.tpp"
+#include "fraction.h"
+#include "fraction.tpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Some simple examples are shown below. These are not exhaustive." << std::endl;
     Fraction<float> a(2, 15);
     Fraction<float> b(1, 5);
-    Fraction<float> c, d, e;
+    Fraction<float> c;
     float item1 = 66;
     c = a / b;
     a = a + item1;
-    b+=a;
+    b += a;
     std::cout << c << std::endl;
     std::cout << b << std::endl;
     std::cout << a << std::endl;
